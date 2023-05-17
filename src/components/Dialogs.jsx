11278a222da5984c88aff4bb3ef2ef8a01c84d6d
P-on <soulshon@yandex.ru>
2023-05-18 00:37:04 +0300

    add send message finally

--- src/components/Dialogs.jsx
+++ src/components/Dialogs.jsx
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { sendMessageCreator} from "./redux/dialogsReducer";
+
 
 const Dialogs = ()=>{
   const dispatch = useDispatch()
   const dialogs  = useSelector(state=>state.dialogsPage.messages)
   const addMessage = (message) =>{
-    const mess = {
+  const mess = {
       message,
-      id: (Math.ceil(Math.random()*80)),
+      id: (Math.ceil(Math.random()*10*80)),
+      
     }
     dispatch(sendMessageCreator(mess))
   }
-  
-  console.log(dialogs)
+  const [inputValue, setInputval] = useState('')
+ 
   
   return(
     <>
@@ -21,7 +23,19 @@ const Dialogs = ()=>{
         dialogs.map(d => <div key={d.id}>{d.id} : {d.message} </div>)
         }</div>
         {/* <input placeholder="введи юзера"/> */}
-          <button onClick={()=>addMessage(prompt(''))}>Send message</button>
+          <button onClick={()=>addMessage(prompt(''))}>Send message (variant 1)</button>
+       <textarea type="text" name="comment" placeholder="Введите комментарий"  value={inputValue} onChange={e=>setInputval(e.target.value)}
+       />
+       
+      <input
+      type="button"
+      onClick={()=>addMessage(inputValue)}
+      className="submitBtn"
+      value={'Enter'}
+
+      />
+       
+      
     </>
   )
     
